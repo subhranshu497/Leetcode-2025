594a1935e7f37612af4b26bd85c17a8561d1a77c
subh <subhranshu497@gmail.com>
2025-02-01 18:36:34 -0700

    02022025-commit

---
+++ src/com/februry/CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,35 @@
+package com.februry;
+
+import java.io.*;
+
+public class CheckIfArrayIsSortedAndRotated {
+    public static void main(String[] args) {
+        int [] nums = {2,1,3,4};
+        boolean flag = check(nums);
+        System.out.println(flag);
+    }
+
+    private static boolean check(int[] nums) {
+        int n = nums.length;
+        int x =-1;
+        for(int i=1;i<n;i++){
+            if (nums[i] >nums[i-1]){
+                continue;
+            }else if(nums[i]< nums[i-1]){
+                x = i;
+                break;
+            }
+        }
+        if(x == -1) return true;
+        int [] b = new int[n];
+        for(int i=0;i<n;i++){
+            b[i] = nums[(i+x)%n];
+        }
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++){
+            if(nums[i] !=b[i])
+                return false;
+        }
+        return true;
+    }
+}
