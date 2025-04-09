122c1b4f7ba24adec325332ef253645973893efc
subh <subhranshu497@gmail.com>
2025-04-08 17:38:38 -0700

    04092025

---
+++ src/com/concept/DFS/MinimumOperationsToMakeArrayValuesEqualToK.java
@@ -0,0 +1,23 @@
+package com.concept.DFS;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MinimumOperationsToMakeArrayValuesEqualToK {
+    public static void main(String[] args) {
+        int [] nums = {9,7,5,3};
+        int k = 1;
+        int ops = minOperations2(nums, k);
+        System.out.println(ops);
+    }
+
+    private static int minOperations2(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+            if(num <k)
+                return -1;
+        }
+        return set.contains(k)?set.size()-1:set.size();
+    }
+}
