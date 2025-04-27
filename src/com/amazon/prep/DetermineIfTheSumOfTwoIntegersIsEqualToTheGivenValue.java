a1a561c2719fe676f5fde021a7ee5ae39d35dd0e
subh <subhranshu497@gmail.com>
2025-04-27 09:32:58 -0700

    04272025

---
+++ src/com/amazon/prep/DetermineIfTheSumOfTwoIntegersIsEqualToTheGivenValue.java
@@ -0,0 +1,9 @@
+package com.amazon.prep;
+
+public class DetermineIfTheSumOfTwoIntegersIsEqualToTheGivenValue {
+    public static void main(String[] args) {
+        int [] nums = {5,7,1,2,8,4,3};
+        int target = 10;
+        //boolean flag = check_two_sum(nums, target);
+    }
+}
