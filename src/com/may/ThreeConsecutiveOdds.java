901a271551a5005d725b591853093fd3a95fbf40
Subh <43628466+subhranshu497@users.noreply.github.com>
2025-05-10 17:26:37 -0700

    05112025

---
+++ src/com/may/ThreeConsecutiveOdds.java
@@ -0,0 +1,10 @@
+public class ThreeConsecutiveOdds {
+  public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        if(n<=2) return false;
+        for(int i=2;i<n;i++){
+            if(arr[i]%2 !=0 && arr[i-1]%2 !=0 && arr[i-2] % 2 !=0)return true;
+        }
+        return false;
+    }
+}
