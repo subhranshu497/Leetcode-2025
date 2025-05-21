73a36a354d13a4a4e412337aab72219c96833f29
subh <subhranshu497@gmail.com>
2025-05-20 21:51:49 -0700

    05212025

--- src/com/may/ZeroArrayTransformationI.java
+++ src/com/may/ZeroArrayTransformationI.java
@@ -4,24 +4,34 @@ import java.util.*;
 
 public class ZeroArrayTransformationI {
     public static void main(String[] args) {
-        int[] nums = {4,3,2,1};
-        int [][] queries = {{1,3},{0,2}};
+        int[] nums = {1,0,1};
+        int [][] queries = {{0,2}};
         boolean flag = isZeroArray(nums, queries);
         System.out.println(flag);
     }
 
     private static boolean isZeroArray(int[] nums, int[][] queries) {
                 int n = nums.length;
-                int overlap[] = new int[n+1];
+                int [] differenceArray = new int[n+1];
+                for(int [] query:queries){
+                    int left = query[0];
+                    int right = query[1];
 
-                for (int i = 0; i < queries.length; i++) {
-                    overlap[queries[i][0]]++;
-                    overlap[queries[i][1]+1]--;
+                    differenceArray[left] +=1;
+                    if(differenceArray[right+1] <n)
+                        differenceArray[right+1] -=1;
                 }
-                int count = 0;
-                for (int i = 0; i < n; i++) {
-                    count += overlap[i];
-                    if (count < nums[i]) return false;
+                //find cumsum
+        int sum =0;
+                int[] cumsum = new int[n];
+                for(int i=0;i<n;i++){
+                    sum +=differenceArray[i];
+                    cumsum[i] = sum;
+                }
+                for(int num:cumsum)
+                    System.out.print(num+", ");
+                for(int i=0;i<n;i++){
+                    if (cumsum[i]< nums[i]) return false;
                 }
                 return true;
     }
