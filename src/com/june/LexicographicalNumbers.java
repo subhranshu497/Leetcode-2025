ae2f4cd35945195de2d536b30ab3d22fed39183c
subh <subhranshu497@gmail.com>
2025-06-08 11:33:11 -0700

    06082025

--- src/com/june/LexicographicalNumbers.java
+++ src/com/june/LexicographicalNumbers.java
@@ -12,10 +12,30 @@ public class LexicographicalNumbers {
         System.out.println(res);
     }
 
+    //using recursion
     private static List<Integer> lexicalOrder(int n) {
         List<Integer> res = new ArrayList<>();
-        int i =1;
-        boolean flag = true;
+        //we have to run a loop as we have to visiteach num and group by its digits
+        for(int num=1;num<=9;num++){
+            solveLexicalOrder(num,n, res);
+        }
+        return res;
+    }
+
+    private static void solveLexicalOrder(int num, int n, List<Integer> res) {
+        //base condition
+        if(num >n) return;
+        res.add(num);
+        //since we have to group all num start with num, i.e 1, 10, 11, 12 ,13...., 2,20, 21....
+        for(int appendDigit=0;appendDigit<=9;appendDigit++){
+            int nextNum = (num*10)+appendDigit;
+            if(nextNum > n) return;
+            solveLexicalOrder(nextNum, n, res);
+        }
+    }
+
+    private static List<Integer> lexicalOrderI(int n) {
+        List<Integer> res = new ArrayList<>();
         res.add(1);
         while (res.size() <n){
             int prevNum = res.getLast();
