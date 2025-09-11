b282bf3db1be54ab6fab80b2f3cb36e94ccab144
subh <subhranshu497@gmail.com>
2025-09-10 21:56:07 -0700

    09112025

--- src/com/sep/SortVowelsinaString.java
+++ src/com/sep/SortVowelsinaString.java
@@ -12,27 +12,17 @@ public class SortVowelsinaString {
     private static String sortVowels(String s) {
         Set<Character> vowelSet = Set.of('A','E','I','O','U','a','e','i','o','u');
         StringBuilder sb = new StringBuilder();
-        Map<Integer, Character> positionOfChar = new TreeMap<>();
         List<Character> vowelList = new ArrayList<>();
-        for(int i=0;i<s.length();i++){
-            if(vowelSet.contains(s.charAt(i))){
-                positionOfChar.put(i,s.charAt(i));
-                vowelList.add(s.charAt(i));
-            }
+        for(int i=0;i<s.length();i++)
+            if(vowelSet.contains(s.charAt(i)))vowelList.add(s.charAt(i));
 
-        }
-        //now take all the vowels and sort those
         Collections.sort(vowelList);
-        //now iterate the String and check for vowel if it is not in right position
-        //take one from the list and delete that one
         for(int i=0;i<s.length();i++) {
             if (vowelSet.contains(s.charAt(i))) {
                 char vowel = vowelList.getFirst();
                 vowelList.removeFirst();
                 sb.append(vowel);
-            }else{
-                sb.append(s.charAt(i));
-            }
+            }else sb.append(s.charAt(i));
         }
         return sb.toString();
     }
